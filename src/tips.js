a9025502c58e4e61854c925d0755471d6e4763be
Tanyiqu <1953649096@qq.com>
2020-12-07 16:21:15 +0800

    上传代码

--- src/tips.js
+++ src/tips.js
@@ -22,6 +22,41 @@
 let tips = $('.tan-tips');
 let tipMain = $('.tan-tips-main');
 
+
+
+// let divHtml = `
+// <div class="tantip">
+// <div class="tantip-show-btn">
+//     赞助
+// </div>
+// <div class="tantip-main">
+//     <div class="tantip-main-header">
+//         <p>喜欢文章就打赏吧</p>
+//     </div>
+
+//     <div class="tantip-main-content">
+
+//     </div>
+
+//     <div class="tantip-main-footer">
+//         <p><a href="" target="_blank">关于插件</a></p>
+//     </div>
+// </div>
+// </div>
+// `;
+
+// let style = document.createElement('link');
+// style.rel = 'stylesheet';
+// style.href = 'tips.css';
+// document.body.appendChild(style);
+
+// let div = document.createElement('div');
+// div.innerHTML = divHtml;
+// document.body.appendChild(div);
+
+
+//插入到最前面
+
 // tips.mouseenter(() => {
 //     tipMain.stop();
 //     tipMain.slideLeftShow(400);
@@ -34,3 +69,29 @@ let tipMain = $('.tan-tips-main');
 
 
 
+// 切换打赏码
+
+let tantipImgDiv = $('.tantip-img-div');
+let tantipImgFrame = $('.tantip-img-frame');
+
+
+
+$('.tantip-alipay').hover(() => {
+    // 切换背景
+    tantipImgDiv.css({
+        'background': 'url(imgs/alipay-frame.png) no-repeat center',
+        'background-size': '60%'
+    });
+    // 切换图片
+    tantipImgFrame.attr('src', 'imgs/ali.png');
+});
+
+$('.tantip-wechat').hover(() => {
+    // 切换背景
+    tantipImgDiv.css({
+        'background': 'url(imgs/wechat-frame.png) no-repeat center',
+        'background-size': '60%'
+    });
+    // 切换图片
+    tantipImgFrame.attr('src', 'imgs/we.png');
+});
