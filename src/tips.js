cb2d8350e85208371a23f77cecbf1c17c6f9d622
Tanyiqu <1953649096@qq.com>
2020-12-07 15:26:00 +0800

    修改样式名称

--- src/tips.js
+++ src/tips.js
@@ -1,56 +1,36 @@
-// class Tips {
-//     constructor(obj) {
-
-//     }
-
-
-
-//     // 挂载到页面
-//     mount() {
-//         let body = $('body');
-
-
-
-
-
-//     }
-
-// }
-
-jQuery.fn.slideLeftHide = function (speed, callback) {
-    this.animate({
-        width: "hide",
-        paddingLeft: "hide",
-        paddingRight: "hide",
-        marginLeft: "hide",
-        marginRight: "hide"
-    }, speed, callback);
-};
-jQuery.fn.slideLeftShow = function (speed, callback) {
-    this.animate({
-        width: "show",
-        paddingLeft: "show",
-        paddingRight: "show",
-        marginLeft: "show",
-        marginRight: "show"
-    }, speed, callback);
-};
+// jQuery.fn.slideLeftHide = function (speed, callback) {
+//     this.animate({
+//         width: "hide",
+//         paddingLeft: "hide",
+//         paddingRight: "hide",
+//         marginLeft: "hide",
+//         marginRight: "hide"
+//     }, speed, callback);
+// };
+// jQuery.fn.slideLeftShow = function (speed, callback) {
+//     this.animate({
+//         width: "show",
+//         paddingLeft: "show",
+//         paddingRight: "show",
+//         marginLeft: "show",
+//         marginRight: "show"
+//     }, speed, callback);
+// };
 
 
-$(function () {
-    let tips = $('.tan-tips');
-    let tipMain = $('.tan-tips-main');
 
-    tips.mouseenter(() => {
-        tipMain.stop();
-        // tipMain.css('display', 'block');
-        tipMain.slideLeftShow(400);
-    });
+let tips = $('.tan-tips');
+let tipMain = $('.tan-tips-main');
 
-    tips.mouseleave(() => {
-        tipMain.stop();
-        // tipMain.css('display', 'none');
-        tipMain.slideLeftHide(400);
-    });
+// tips.mouseenter(() => {
+//     tipMain.stop();
+//     tipMain.slideLeftShow(400);
+// });
 
-});
+// tips.mouseleave(() => {
+//     tipMain.stop();
+//     tipMain.slideLeftHide(400);
+// });
+
+
+
