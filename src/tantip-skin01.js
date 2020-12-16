39c3972d31a50cce7f75cd6eba63724a175b2fa1
Tanyiqu <1953649096@qq.com>
2020-12-16 16:48:29 +0800

    完成1.1版测试

--- src/tantip-skin01.js
+++ src/tantip-skin01.js
@@ -1,46 +1,42 @@
-const tantip_version = 'v1.0';
+const tantip_version = 'v1.1';
 
 const alipay_frame_url = 'https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/alipay-frame.png';
 const wechat_frame_url = 'https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/wechat-frame.png';
 const alipay_text_utl = 'https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/alipay-text.png';
 const wechat_text_utl = 'https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/wechat-text.png';
 
-const tantip_css = 'https://tanyiqu.gitee.io/tantip-cnblogs/dist/1.0/tantip.css';
-
+const tantip_css = './tantip-skin01.css';
 
 const tantip_html = `
-<div id="tantip">
-<div id="tantip-btn">
-</div>
-
-<div id="tantip-main" style="display: none;">
-    <div id="tantip-main-header">
-        <p id="tantip-main-text" class="tantip-main-p"></p>
+<div id="tantip-skin01">
+    <div class="tantip-btn">
     </div>
 
-    <div id="tantip-main-content">
-        <div id="tantip-main-content-img-div">
-            <img id="tantip-img-frame" src="https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/ali.png" alt="">
+    <div class="tantip-main">
+        <div class="tantip-main-header">
         </div>
 
-        <div id="tantip-main-content-chimg">
-            <div id="ch2alpay" class="tantip-main-content-chimg-item">
-                <img class="tantip-chimg-item-img"
-                    src="${alipay_text_utl}" alt="">
+        <div class="tantip-main-content">
+            <div id="qr-frame">
+                <img id="tantip-img-frame" src="https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/ali.png" alt="">
             </div>
-            <div id="ch2wechat" class="tantip-main-content-chimg-item">
-                <img class="tantip-chimg-item-img"
-                    src="${wechat_text_utl}" alt="">
+
+            <div id="change-qr">
+                <div id="ch2alpay" class="qr-item">
+                    <img class="qr-item-img" src="${alipay_text_utl}" alt="">
+                </div>
+                <div id="ch2wechat" class="qr-item">
+                    <img class="qr-item-img" src="${wechat_text_utl}" alt="">
+                </div>
             </div>
         </div>
-    </div>
 
-    <div id="tantip-main-footer">
-        <p class="tantip-main-p">
-            <a class="tantip-main-p-a" href="https://github.com/tanyiqu/tantip-cnblogs" target="_blank">关于插件 ${tantip_version}</a>
-        </p>
+        <div class="tantip-main-footer">
+            <p>
+                <a href="https://github.com/tanyiqu/tantip-cnblogs" target="_blank">关于插件 ${tantip_version}</a>
+            </p>
+        </div>
     </div>
-</div>
 </div>
 `;
 
@@ -51,17 +47,19 @@ let tantip_config = {
     wechat: 'https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/we.png',
 };
 
+
 class Tantip {
     constructor(config) {
         this.config = config;
     }
 
+
     /**
      * 加载tantip的事件
      */
     loadEvent() {
-        let tantip = document.querySelector('#tantip');
-        let tantip_main = document.querySelector('#tantip-main');
+        let tantip = document.querySelector('#tantip-skin01');
+        let tantip_main = document.querySelector('.tantip-main');
         // 鼠标移入移除事件
         {
             tantip.onmouseenter = () => {
@@ -77,11 +75,12 @@ class Tantip {
         {
             let ch2alpay = document.querySelector('#ch2alpay');
             let ch2wechat = document.querySelector('#ch2wechat');
-            let imgDiv = document.querySelector('#tantip-main-content-img-div');
+            // 二维码框
+            let imgDiv = document.querySelector('#qr-frame');
+            // 二维码
             let qr = document.querySelector('#tantip-img-frame');
 
             ch2alpay.onmouseenter = () => {
-                // imgDiv.style.backgroundImage = "url(https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/alipay-frame.png)";
                 imgDiv.style.backgroundImage = `url(${alipay_frame_url})`;
                 imgDiv.style.backgroundRepeat = "no-repeat";
                 imgDiv.style.backgroundPosition = "center";
@@ -90,7 +89,6 @@ class Tantip {
             };
 
             ch2wechat.onmouseenter = () => {
-                // imgDiv.style.backgroundImage = "url(https://tanyiqu.gitee.io/tantip-cnblogs/src/imgs/wechat-frame.png)";
                 imgDiv.style.backgroundImage = `url(${wechat_frame_url})`;
                 imgDiv.style.backgroundRepeat = "no-repeat";
                 imgDiv.style.backgroundPosition = "center";
@@ -102,20 +100,20 @@ class Tantip {
 
     mount() {
         // 追加样式
-        // let style = document.createElement('link');
-        // style.rel = 'stylesheet';
-        // style.href = tantip_css;
-        // document.body.appendChild(style);
+        let style = document.createElement('link');
+        style.rel = 'stylesheet';
+        style.href = tantip_css;
+        document.body.appendChild(style);
 
         // 挂载到body里面
-        // let tantip = document.createElement('div');
-        // tantip.innerHTML = tantip_html;
-        // document.body.appendChild(tantip);
+        let tantip = document.createElement('div');
+        tantip.innerHTML = tantip_html;
+        document.body.appendChild(tantip);
 
         // 按钮文字
         let title_chars = this.config.title.split('');
         console.log(title_chars);
-        let tantip_btn = document.querySelector('#tantip-btn');
+        let tantip_btn = document.querySelector('.tantip-btn');
         let html = '';
         title_chars.forEach(str => {
             html += `<p class="tantip-btn-p">${str}</p>`;
@@ -123,9 +121,11 @@ class Tantip {
         tantip_btn.innerHTML = html;
 
         // 上面一行小字
-        document.querySelector('#tantip-main-text').innerHTML = this.config.text;
-
+        document.querySelector('.tantip-main-header').innerHTML = `
+            <p>${this.config.text}</p>
+        `;
 
         this.loadEvent();
     }
+
 }
